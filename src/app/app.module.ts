187db90c300eb58294e4f2328df059ae694e414a
Rakia Ben Sassi <rakia.bensassi@gmail.com>
2020-11-22 13:35:37 +0100

    import MatCheckboxModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatTableModule
   ],
   providers: [],
